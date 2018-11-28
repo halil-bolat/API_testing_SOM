171bf7a0941218fe9e485eae20831e6ce2b0e638
halil-bolat <hbolat@spartaglobal.com>
2018-11-28 10:58:41 +0000

    first commit, structure is builtn with a dummy test that fails

---
+++ lib/services/multiple_postcode_service.rb
@@ -0,0 +1,16 @@
+require 'httparty'
+require 'json'
+
+
+class MultiplePostcodesService
+  include HTTParty
+
+  base_uri 'https://api.postcodes.io'
+
+  def get_multiple_postcodes(postcodes_array)
+
+
+
+  end
+
+end
---
+++ lib/services/single_postcode_service.rb
@@ -0,0 +1,16 @@
+require 'httparty'
+require 'json'
+
+
+class SinglePostcodeService
+  include HTTParty
+
+  base_uri 'https://api.postcodes.io'
+
+  def get_single_postcode(postcode)
+
+        
+
+  end
+
+end
